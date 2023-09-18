cb95d0c0e7fd5d7219a625850ee95e1cbb775628
Lasse Fuglsbjerg <lassevestergaardf@gmail.com>
2023-09-18 13:33:21 +0200

    changes to Weather

--- src/App/Weather.test.tsx
+++ src/App/Weather.test.tsx
@@ -1,8 +1,12 @@
-import { render } from "@testing-library/react"
-import { Weather } from "./Weather"
+import { render } from "@testing-library/react";
+import { Weather } from "./Weather";
 
-describe( "Weather", () => {
-    it( "Renders Component According To Snapshot", () => {
-        const { container } = render( <Weather /> )
-        
-        expect( container ).toMatchSnapshot() } ) } )
+describe("Weather", () => {
+  it("Renders Component According To Snapshot", () => {
+    //Change: Small changes to the Weather test although testing isn't my field of expertise. Changes made as the city prop is no longer used.
+    const testCity = "Paris";
+    const { container } = render(<Weather city={testCity} />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
--- src/App/Weather.tsx
+++ src/App/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //Change: Typesafety
 
@@ -16,50 +16,51 @@ const API_KEY = "4c4f0b1876954338598a7be96c66527b";
 const API_URL = "https://api.openweathermap.org";
 
 //Change: Use of defined types
-const Weather = (props: WeatherProps) => {
+const Weather: React.FC<WeatherProps> = ({ city }) => {
   const [info, setInfo] = useState<WeatherInfo>({
     city: null,
     temp: null,
     icon: null,
   });
 
-  const isPending = React.useRef(false);
+  //Change: Wrapping the API call inside a useEffect to ensure weather data is fetched whenever the city prop changes.
+  // I am also using async/await. Readability but also makes it more clear that one call depends on previous.
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!city) return;
 
-  React.useLayoutEffect(() => {
-    if (props.city !== null || isPending.current === false) {
-      isPending.current = true;
+      //Change: Try/Catch statement for error handling
+      try {
+        const geoResponse = await fetch(
+          `${API_URL}/geo/1.0/direct?q=${city}&appid=${API_KEY}`
+        );
+        const geoData = await geoResponse.json();
 
-      fetch(`${API_URL}/geo/1.0/direct?q=${props.city}&appid=${API_KEY}`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
+        const weatherResponse = await fetch(
+          `${API_URL}/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=metric&appid=${API_KEY}`
+        );
 
-          fetch(
-            `${API_URL}/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=4c4f0b1876954338598a7be96c66527b`
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
+        //Change: new variable to use in setInfo
+        const weatherData = await weatherResponse.json();
 
-              setInfo({
-                city: data.name,
-                temp: data.main.temp,
-                icon: `${API_URL}/img/wn/${data.weather[0].icon}@2x.png` as any,
-              });
-
-              isPending.current = false;
-            });
+        setInfo({
+          city: weatherData.name,
+          temp: weatherData.main.temp,
+          icon: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`,
         });
-    }
-  }, [props.city]);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city]);
 
   return (
     <>
       <h1>{info.city}</h1>
-
       <p>{info.temp && ~~info.temp} Celcius</p>
-
-      <img src={info.icon as unknown as string} alt="Icon" />
+      <img src={info.icon ? info.icon : ""} alt="Weather Icon" />
     </>
   );
 };
